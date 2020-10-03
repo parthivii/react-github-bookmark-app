a648bc40a9b775d6f308643070cb79210916203a
parthivii <parthivisrivastava14@gmail.com>
2020-10-04 02:10:57 +0530

    Updates

---
+++ precache-manifest.7eb81c145143111f7d2a61356f41044c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d02c4e397f73994d0bf5ab9876949f2",
+    "url": "/react-github-bookmark-app/index.html"
+  },
+  {
+    "revision": "c8107371b59602fb819b",
+    "url": "/react-github-bookmark-app/static/css/main.1acc86c3.chunk.css"
+  },
+  {
+    "revision": "2a6a3fccbe304d327cbf",
+    "url": "/react-github-bookmark-app/static/js/2.c945f5e9.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/react-github-bookmark-app/static/js/2.c945f5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8107371b59602fb819b",
+    "url": "/react-github-bookmark-app/static/js/main.15f740c7.chunk.js"
+  },
+  {
+    "revision": "06eb92565301c3c65b7c",
+    "url": "/react-github-bookmark-app/static/js/runtime-main.0c53c940.js"
+  }
+]);
